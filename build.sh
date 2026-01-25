6f995388609eb4f633ef2053946fd89a6d8baa88
Tanimowo David <davidtanimowo01@gmail.com>
2026-01-25 20:11:06 +0100

    Remove static files collection from build script
    Removed the command to collect static files for the admin panel.

--- build.sh
+++ build.sh
@@ -5,8 +5,5 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files (needed for admin panel CSS)
-python manage.py collectstatic --no-input
-
 # Run migrations
 python manage.py migrate
