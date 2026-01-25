306e2003c7a89cb340b234491d2db4789395e336
David <davidtanimowo01@gmail.com>
2026-01-25 10:15:23 +0100

    modified allowed host

--- aura_core/settings.py
+++ aura_core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aura-finance-weyy.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
